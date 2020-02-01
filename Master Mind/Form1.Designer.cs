548bac42a539afdd63767db81235a54e5cd9f2d6
Jacob <jacobjohnschnell@gmail.com>
2020-02-01 16:27:05 -0500

    Add project files.

---
+++ Master Mind/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Master_Mind
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.picBoard = new System.Windows.Forms.PictureBox();
+            this.picPegs = new System.Windows.Forms.PictureBox();
+            this.picAnswer = new System.Windows.Forms.PictureBox();
+            this.txtRules = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnPink = new System.Windows.Forms.Button();
+            this.btnOrange = new System.Windows.Forms.Button();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.picColor = new System.Windows.Forms.PictureBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.chkDuplicates = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPegs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoard
+            // 
+            this.picBoard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.picBoard, "picBoard");
+            this.picBoard.Name = "picBoard";
+            this.picBoard.TabStop = false;
+            this.picBoard.Click += new System.EventHandler(this.PicBoard_Click);
+            this.picBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.PicBoard_Paint);
+            // 
+            // picPegs
+            // 
+            this.picPegs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.picPegs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.picPegs, "picPegs");
+            this.picPegs.Name = "picPegs";
+            this.picPegs.TabStop = false;
+            this.picPegs.Paint += new System.Windows.Forms.PaintEventHandler(this.PicPegs_Paint);
+            // 
+            // picAnswer
+            // 
+            this.picAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.picAnswer, "picAnswer");
+            this.picAnswer.Name = "picAnswer";
+            this.picAnswer.TabStop = false;
+            this.picAnswer.Paint += new System.Windows.Forms.PaintEventHandler(this.PicAnswer_Paint);
+            // 
+            // txtRules
+            // 
+            this.txtRules.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtRules, "txtRules");
+            this.txtRules.Name = "txtRules";
+            this.txtRules.ReadOnly = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LightBlue;
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnRed
+            // 
+            resources.ApplyResources(this.btnRed, "btnRed");
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Name = "btnRed";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // btnBlue
+            // 
+            resources.ApplyResources(this.btnBlue, "btnBlue");
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // btnGreen
+            // 
+            resources.ApplyResources(this.btnGreen, "btnGreen");
+            this.btnGreen.BackColor = System.Drawing.Color.Green;
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // btnYellow
+            // 
+            resources.ApplyResources(this.btnYellow, "btnYellow");
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // btnPink
+            // 
+            resources.ApplyResources(this.btnPink, "btnPink");
+            this.btnPink.BackColor = System.Drawing.Color.HotPink;
+            this.btnPink.Name = "btnPink";
+            this.btnPink.UseVisualStyleBackColor = false;
+            this.btnPink.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // btnOrange
+            // 
+            resources.ApplyResources(this.btnOrange, "btnOrange");
+            this.btnOrange.BackColor = System.Drawing.Color.Orange;
+            this.btnOrange.Name = "btnOrange";
+            this.btnOrange.UseVisualStyleBackColor = false;
+            this.btnOrange.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.BackColor = System.Drawing.Color.Lime;
+            resources.ApplyResources(this.btnValidate, "btnValidate");
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.UseVisualStyleBackColor = false;
+            this.btnValidate.Click += new System.EventHandler(this.BtnValidate_Click);
+            // 
+            // btnErase
+            // 
+            this.btnErase.BackColor = System.Drawing.Color.LightCoral;
+            resources.ApplyResources(this.btnErase, "btnErase");
+            this.btnErase.Name = "btnErase";
+            this.btnErase.UseVisualStyleBackColor = false;
+            this.btnErase.Click += new System.EventHandler(this.BtnErase_Click);
+            // 
+            // picColor
+            // 
+            this.picColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.picColor, "picColor");
+            this.picColor.Name = "picColor";
+            this.picColor.TabStop = false;
+            this.picColor.Paint += new System.Windows.Forms.PaintEventHandler(this.PicColor_Paint);
+            // 
+            // lblColor
+            // 
+            resources.ApplyResources(this.lblColor, "lblColor");
+            this.lblColor.Name = "lblColor";
+            // 
+            // chkDuplicates
+            // 
+            resources.ApplyResources(this.chkDuplicates, "chkDuplicates");
+            this.chkDuplicates.Name = "chkDuplicates";
+            this.chkDuplicates.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chkDuplicates);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.picColor);
+            this.Controls.Add(this.btnErase);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.btnOrange);
+            this.Controls.Add(this.btnPink);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtRules);
+            this.Controls.Add(this.picAnswer);
+            this.Controls.Add(this.picPegs);
+            this.Controls.Add(this.picBoard);
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FrmMain_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPegs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picColor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoard;
+        private System.Windows.Forms.PictureBox picPegs;
+        private System.Windows.Forms.PictureBox picAnswer;
+        private System.Windows.Forms.RichTextBox txtRules;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnPink;
+        private System.Windows.Forms.Button btnOrange;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Button btnErase;
+        private System.Windows.Forms.PictureBox picColor;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.CheckBox chkDuplicates;
+    }
+}
+
---
+++ Master Mind/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Master_Mind
+{
+    public partial class frmMain : Form
+    {
+        int[][] rows = new int[10][];
+        int[,] guesses = new int[10, 2];
+        int[] answer = new int[4];
+        int currentRow = 0;
+        int currentColor = 1;
+        bool solved = false;
+        bool playing = false;
+        DateTime started;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private int randInt(int min, int max)
+        {
+            MsdnMag.CryptoRandom random = new MsdnMag.CryptoRandom();
+            return random.Next(min, max);
+        }
+
+        private void enable(bool enabled)
+        {
+            btnRed.Enabled = enabled;
+            btnBlue.Enabled = enabled;
+            btnGreen.Enabled = enabled;
+            btnYellow.Enabled = enabled;
+            btnPink.Enabled = enabled;
+            btnOrange.Enabled = enabled;
+            btnValidate.Enabled = enabled;
+            btnErase.Enabled = enabled;
+            lblColor.Visible = enabled;
+            picColor.Visible = enabled;
+            solved = !enabled;
+        }
+
+        private void PicBoard_Paint(object sender, PaintEventArgs e)
+        {
+            Brush[] colors = { Brushes.LightGray, Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.Yellow, Brushes.HotPink, Brushes.Orange };
+            for (int i = 0; i < 10; i++)
+            {
+                for(int k = 0; k < 4; k++)
+                {
+                    e.Graphics.FillEllipse(colors[rows[i][k]], k * 50 + 10, i * 50 + 10, 40, 40);
+                    e.Graphics.DrawEllipse(Pens.Black, k * 50 + 10, i * 50 + 10, 40, 40);
+                }
+            }
+        }
+
+        private void PicPegs_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int parsed = 0;
+                for (int k = 0; k < guesses[i, 1]; k++)
+                {
+                    e.Graphics.FillEllipse(Brushes.Black, parsed % 2 * 25 + 5, parsed / 2 * 25 + 5 + i * 50, 20, 20);
+                    e.Graphics.DrawEllipse(Pens.Black, parsed % 2 * 25 + 5, parsed / 2 * 25 + 5 + i * 50, 20, 20);
+                    parsed++;
+                }
+                for (int k = 0; k < guesses[i, 0]; k++)
+                {
+                    e.Graphics.FillEllipse(Brushes.White, parsed % 2 * 25 + 5, parsed / 2 * 25 + 5 + i * 50, 20, 20);
+                    e.Graphics.DrawEllipse(Pens.Black, parsed % 2 * 25 + 5, parsed / 2 * 25 + 5 + i * 50, 20, 20);
+                    parsed++;
+                }
+                for (int k = parsed; k < 4; k++)
+                {
+                    e.Graphics.FillEllipse(Brushes.LightGray, parsed % 2 * 25 + 5, parsed / 2 * 25 + 5 + i * 50, 20, 20);
+                    e.Graphics.DrawEllipse(Pens.Black, parsed % 2 * 25 + 5, parsed / 2 * 25 + 5 + i * 50, 20, 20);
+                    parsed++;
+                }
+            }
+        }
+
+        private void PicAnswer_Paint(object sender, PaintEventArgs e)
+        {
+            if (playing)
+            {
+                if (solved)
+                {
+                    Brush[] colors = { Brushes.LightGray, Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.Yellow, Brushes.HotPink, Brushes.Orange };
+                    for (int i = 0; i < 4; i++)
+                    {
+                        e.Graphics.FillEllipse(colors[answer[i]], i * 50 + 10, 10, 40, 40);
+                        e.Graphics.DrawEllipse(Pens.Black, i * 50 + 10, 10, 40, 40);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        e.Graphics.DrawImage(Properties.Resources.qm, i * 50 + 10, 10, 40, 40);
+                    }
+                }
+            } else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    e.Graphics.FillEllipse(Brushes.LightGray, i * 50 + 10, 10, 40, 40);
+                    e.Graphics.DrawEllipse(Pens.Black, i * 50 + 10, 10, 40, 40);
+                }
+            }
+        }
+
+        private void PicColor_Paint(object sender, PaintEventArgs e)
+        {
+            Brush[] colors = { Brushes.LightGray, Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.Yellow, Brushes.HotPink, Brushes.Orange };
+            e.Graphics.FillRectangle(colors[currentColor], 0, 0, 25, 25);
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                rows[i] = new int[4];
+                for (int k = 0; k < 4; k++)
+                {
+                    rows[i][k] = 0;
+                    guesses[i, k/2] = 0;
+                }
+            }
+            enable(false);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            started = DateTime.Now;
+            for (int i = 0; i < 4; i++)
+            {
+                int ri = randInt(1, 6);
+                while (answer.Contains(ri) && chkDuplicates.Checked == false)
+                {
+                    ri = randInt(1, 6);
+                    
+                }
+                answer[i] = ri;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    rows[i][k] = 0;
+                    guesses[i, k/2] = 0;
+                }
+            }
+            currentRow = 0;
+            playing = true;
+            enable(true);
+            picBoard.Invalidate();
+            picPegs.Invalidate();
+            picColor.Invalidate();
+            picAnswer.Invalidate();
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            currentColor = int.Parse((sender as Button).AccessibleDescription);
+            picColor.Invalidate();
+        }
+
+        private void PicBoard_Click(object sender, EventArgs e)
+        {
+            if (!playing) { return; }
+            int inverseRow = 10 - currentRow - 1;
+            int x = (Cursor.Position.X - this.DesktopLocation.X - 8);
+            int y = (Cursor.Position.Y - this.DesktopLocation.Y - 30);
+            if (y >= inverseRow * 50 + 10 && y <= inverseRow * 50 + 60)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (x >= i * 50 + 10 && x <= i * 50 + 60)
+                    {
+                        rows[inverseRow][i] = currentColor;
+                    }
+                }
+            }
+            picBoard.Invalidate();
+        }
+
+        private void BtnValidate_Click(object sender, EventArgs e)
+        {
+            int inverseRow = 10 - currentRow - 1;
+            int[] testedRow = rows[inverseRow];
+            if (!testedRow.Contains(0))
+            {
+                int whiteCount = 0;
+                int blackCount = 0;
+                testedRow = testedRow.Distinct().ToArray();
+                foreach(int i in testedRow)
+                {
+                    whiteCount += Math.Min(rows[inverseRow].Count(n => n == i), answer.Count(n => n == i));
+                }
+                for(int i = 0; i < 4; i++)
+                {
+                    if (rows[inverseRow][i] == answer[i])
+                    {
+                        blackCount++;
+                        whiteCount--;
+                    }
+                }
+                guesses[inverseRow, 0] = whiteCount;
+                guesses[inverseRow, 1] = blackCount;
+                currentRow++;
+                picPegs.Invalidate();
+                if (currentRow >= 10)
+                {
+                    MessageBox.Show("Tu as perdu! :(");
+                    solved = true;
+                    picAnswer.Invalidate();
+                } else if (blackCount == 4)
+                {
+                    TimeSpan timeTaken = new TimeSpan(DateTime.Now.Ticks - started.Ticks);
+                    MessageBox.Show(string.Format("Tu as gagné! :) {0}Tu as gagné en {1} essais, ce qui t'as pris {2}", Environment.NewLine, currentRow, timeTaken.ToString(@"mm\:ss")));
+                    solved = true;
+                    picAnswer.Invalidate();
+                }
+            }
+        }
+
+        private void FrmMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int prevColor = currentColor;
+            try
+            {
+                currentColor = int.Parse(e.KeyChar.ToString());
+                if (currentColor < 1 || currentColor > 6)
+                {
+                    currentColor = prevColor;
+                }
+                picColor.Invalidate();
+            } catch (Exception ex)
+            {
+                currentColor = prevColor;
+            }
+            e.Handled = true;
+        }
+
+        private void BtnErase_Click(object sender, EventArgs e)
+        {
+            int inverseRow = 10 - currentRow - 1;
+            for (int i = 0; i < 4; i++)
+            {
+                rows[inverseRow][i] = 0;
+            }
+            picBoard.Invalidate();
+        }
+    }
+}
+
+namespace MsdnMag
+{
+    public class CryptoRandom : Random
+    {
+        private RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
+        private byte[] _uint32Buffer = new byte[4];
+
+        public CryptoRandom() { }
+        public CryptoRandom(Int32 ignoredSeed) { }
+
+        public override Int32 Next()
+        {
+            _rng.GetBytes(_uint32Buffer);
+            return BitConverter.ToInt32(_uint32Buffer, 0) & 0x7FFFFFFF;
+        }
+
+        public override Int32 Next(Int32 maxValue)
+        {
+            if (maxValue < 0) throw new ArgumentOutOfRangeException("maxValue");
+            return Next(0, maxValue);
+        }
+
+        public override Int32 Next(Int32 minValue, Int32 maxValue)
+        {
+            if (minValue > maxValue) throw new ArgumentOutOfRangeException("minValue");
+            if (minValue == maxValue) return minValue;
+            Int64 diff = maxValue - minValue;
+
+            while (true)
+            {
+                _rng.GetBytes(_uint32Buffer);
+                UInt32 rand = BitConverter.ToUInt32(_uint32Buffer, 0);
+
+                Int64 max = (1 + (Int64)UInt32.MaxValue);
+                Int64 remainder = max % diff;
+                if (rand < max - remainder)
+                {
+                    return (Int32)(minValue + (rand % diff));
+                }
+            }
+        }
+
+        public override double NextDouble()
+        {
+            _rng.GetBytes(_uint32Buffer);
+            UInt32 rand = BitConverter.ToUInt32(_uint32Buffer, 0);
+            return rand / (1.0 + UInt32.MaxValue);
+        }
+
+        public override void NextBytes(byte[] buffer)
+        {
+            if (buffer == null) throw new ArgumentNullException("buffer");
+            _rng.GetBytes(buffer);
+        }
+    }
+}
